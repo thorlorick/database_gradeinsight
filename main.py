86ef71e7e5e94ab366e2f24e1b7925fa347249e4
ThorloRick <thorlorick@gmail.com>
2025-06-05 14:50:29 -0400

    Create main.py
    Started the main script

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import os
+
+app = FastAPI()
+
+@app.get("/download-template")
+def download_template():
+    file_path = "template.csv"
+    if os.path.exists(file_path):
+        return FileResponse(path=file_path, filename="grade_insight_template.csv", media_type='text/csv')
+    else:
+        return {"error": "Template file not found."}
