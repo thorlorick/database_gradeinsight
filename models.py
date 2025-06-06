9123f103d4a21f81047ef8d65f4cb96c2b213b0b
ThorloRick <thorlorick@gmail.com>
2025-06-06 17:16:03 -0400

    Create models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Student(Base):
+    __tablename__ = "students"
+
+    id = Column(Integer, primary_key=True, index=True)
+    student_number = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
