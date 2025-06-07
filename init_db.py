c06f7e72e96fcee2a03bf4fea0c2d2a0de1c26ac
ThorloRick <thorlorick@gmail.com>
2025-06-06 20:40:18 -0400

    Create init_db.py
    This script will connect to the database and create all tables defined in models.py

---
+++ init_db.py
@@ -0,0 +1,6 @@
+from database import Base, engine
+from models import Student, Assignment, Grade  # Import any other models you’ve added
+
+print("Creating tables...")
+Base.metadata.create_all(bind=engine)
+print("Done.")
