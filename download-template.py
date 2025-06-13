53640f9f285cc9b762063a367bf2080efacf5f4e
ThorloRick <thorlorick@gmail.com>
2025-06-13 12:59:41 -0400

    Update download-template.py

--- download-template.py
+++ download-template.py
@@ -3,3 +3,12 @@ from fastapi import APIRouter
 router = APIRouter()
 
 @router.get("/download-template")
+def download_template():
+    file_path = "template.csv"
+    if os.path.exists(file_path):
+        return FileResponse(path=file_path, filename="grade_insight_template.csv", media_type='text/csv')
+    else:
+        return JSONResponse(
+            status_code=404,
+            content={"error": "Template file not found."}
+        )
