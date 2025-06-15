82697da33a5ff0ab7db929bac9227b2c6347b266
ThorloRick <thorlorick@gmail.com>
2025-06-14 20:47:31 -0400

    Update downloadTemplate.py

--- downloadTemplate.py
+++ downloadTemplate.py
@@ -8,7 +8,11 @@ router = APIRouter()
 def download_template():
     file_path = "template.csv"
     if os.path.exists(file_path):
-        return FileResponse(path=file_path, filename="grade_insight_template.csv", media_type='text/csv')
+        return FileResponse(
+            path=file_path, 
+            filename="grade_insight_template.csv", 
+            media_type='text/csv'
+        )
     else:
         return JSONResponse(
             status_code=404,
