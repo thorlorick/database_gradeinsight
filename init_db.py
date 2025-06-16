2716a6b1e52834e06f0231aece141f5f75b494cc
ThorloRick <thorlorick@gmail.com>
2025-06-16 10:56:24 -0400

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -5,7 +5,4 @@ print("Creating tables...")
 Base.metadata.create_all(bind=engine)
 print("Done.")
 
-@app.get("/init-db")
-def init_db():
-    Base.metadata.create_all(bind=engine)
-    return {"message": "Database tables created."}
+
