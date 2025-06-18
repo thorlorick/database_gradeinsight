05f4d64b4480acd7252188ec06264674787fb4c1
ThorloRick <thorlorick@gmail.com>
2025-06-18 17:09:53 -0400

    Delete routes/api directory

--- routes/api/_init_.py
+++
@@ -1,6 +0,0 @@
-# routes/api/__init__.py
-from fastapi import APIRouter
-from . import students  # import more files as you split them
-
-router = APIRouter()
-router.include_router(students.router)
